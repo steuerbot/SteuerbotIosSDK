3392157c26420a2bc7e539121c720c3e2043da8d
Benjamin <dev@kramser.xyz>
2022-01-16 23:40:04 +0100

    init

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Steuerbot",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Steuerbot",
+            targets: ["SwiftFramework", "hermes"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        // No depency path added
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        
+        .binaryTarget(
+            name: "SwiftFramework",
+            url: "https://sb.kramser.xyz/SwiftFramework.xcframework.zip",
+            checksum: "8ba01fe1d99bf847171d69a867dc35311e284dc893b415353dbde4c8759d7a37"
+        ),
+        .binaryTarget(
+            name: "hermes",
+            url: "https://sb.kramser.xyz/hermes.xcframework.zip",
+            checksum: "53a9b586848fb4529f86920756e6273435e1b5ef247d8311e1ae0ed444e38a66"
+        ),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,34 @@
+# Steuerbot iOS SDK
+
+This Swift Package allows you to add the Steuerbot SDK to your iOS App.
+
+## Installation
+
+### Add the Swift Package to your Project
+Add the package by selecting `File` → `Add Packages…` in Xcode’s menu bar. Search for the Steuerbot SDK using the repo's URL:
+
+```console
+https://github.com/steuerbot/SteuerbotIosSDK.git
+```
+
+Then, select `Add Package`.
+
+### Embed the Framework
+
+In your Apps target add `Steuerbot` to `Frameworks, Libraries and Embedded Content` 
+
+
+## Usage
+
+Present Steuerbot from your ViewController
+```swift
+import Steuerbot
+...
+let framework = SteuerbotSDK()
+let controller = UIViewController()
+controller.view = framework.getView()
+self.present(controller, animated: true, completion: nil)
+```
+
+## License
+[TODO](https://todo.de)
