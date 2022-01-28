49ae105d2e571ef4fb241f2a9ef03cb48a14ad83
benjamin <benjamin@steuerbot.com>
2022-01-28 15:53:30 +0100

    fix user in example

--- Example/example/example/SteuerbotViewController.swift
+++ Example/example/example/SteuerbotViewController.swift
@@ -10,11 +10,10 @@ import Steuerbot
 
 var framework: SteuerbotSDK?
 
-let user = User(email: "sdk01@byom.de", forename: "Max", surname: "Power")
-
 class SteuerbotViewController: UIViewController {
 
     override func viewDidLoad() {
+        let user = User(email: "sdk01@byom.de", forename: "Max", surname: "Power")
         framework = SteuerbotSDK(partnerId: "vivid", token: "testing", user: user)
         self.view = framework?.getView()
         super.viewDidLoad()
