e1b46cb6e22e91a464ad28c8472aa4676bb2f9d6
Benjamin Kramser <dev@kramser.xyz>
2022-02-08 16:52:29 +0100

    v1.0.0

--- Package.swift
+++ Package.swift
@@ -25,12 +25,12 @@ let package = Package(
         
         .binaryTarget(
             name: "SwiftFramework",
-            url: "https://steuerbot-app-artifacts-nonprod.s3.eu-central-1.amazonaws.com/sdk/ios/v1.0.0-rc.3/SwiftFramework.xcframework.zip",
-            checksum: "682d40b2e1518a26f0b8e004b8143cd8bd8f4c2c66c9bece2e5ea7f42acb27ba"
+            url: "https://steuerbot-app-artifacts-nonprod.s3.eu-central-1.amazonaws.com/sdk/ios/v1.0.0/SwiftFramework.xcframework.zip",
+            checksum: "397fa8f12179c01015321e83a14eb6ced4d7145984596f1bd7ae4f84ba4b7b1e"
         ),
         .binaryTarget(
             name: "hermes",
-            url: "https://steuerbot-app-artifacts-nonprod.s3.eu-central-1.amazonaws.com/sdk/ios/v1.0.0-rc.3/hermes.xcframework.zip",
+            url: "https://steuerbot-app-artifacts-nonprod.s3.eu-central-1.amazonaws.com/sdk/ios/v1.0.0/hermes.xcframework.zip",
             checksum: "53a9b586848fb4529f86920756e6273435e1b5ef247d8311e1ae0ed444e38a66"
         ),
     ]
