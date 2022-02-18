597df738454cd7befb6376239e58b59b37041873
Marc Neumann <marc@steuerbot.com>
2022-02-18 17:29:31 +0100

    1.0.3-SNAPSHOT
    5ae9198

--- Package.swift
+++ Package.swift
@@ -26,11 +26,11 @@ let package = Package(
         .binaryTarget(
             name: "SwiftFramework",
             url: "https://steuerbot-app-artifacts-nonprod.s3.eu-central-1.amazonaws.com/sdk/ios/1.0.3-SNAPSHOT/SwiftFramework.xcframework.zip",
-            checksum: "1cb644eb2d0f1293e65349c7e99b9395e744b2c2c68919a207fce555dbf8c02a"
+            checksum: "ebc19f8149728161643fcb969755ec1b03e5576ffee59e6c66ff04d5c621a96f"
         ),
         .binaryTarget(
             name: "hermes",
-            url: "https://steuerbot-app-artifacts-nonprod.s3.eu-central-1.amazonaws.com/sdk/ios/v1.0.2/hermes.xcframework.zip",
+            url: "https://steuerbot-app-artifacts-nonprod.s3.eu-central-1.amazonaws.com/sdk/ios/hermes/v0.66.3/hermes.xcframework.zip",
             checksum: "53a9b586848fb4529f86920756e6273435e1b5ef247d8311e1ae0ed444e38a66"
         ),
     ]
